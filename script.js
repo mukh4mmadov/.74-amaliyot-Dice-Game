d4841209793b9316e07b77cbb0098f8d19c09a63
mukh4mmadov <omuhammadov467@gmail.com>
2024-12-05 22:48:54 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const btnRoll = document.querySelector(".btn--roll");
+const btnNew = document.querySelector(".btn--new");
+const btnHold = document.querySelector(".btn--hold");
+const diceImg = document.querySelector(".dice");
+
+let currentScore = 0;
+let activePlayer = 0;
+let score = [0, 0];
+let gameOver = true;
+
+const switchPlayer = () => {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  document.querySelector(".player--0").classList.toggle("player--active");
+  document.querySelector(".player--1").classList.toggle("player--active");
+};
+
+diceImg.style.display = "none";
+btnRoll.addEventListener("click", () => {
+  if (gameOver) {
+    diceImg.style.display = "block";
+
+    const random = Math.floor(Math.random() * 6 + 1);
+    diceImg.src = `./dice-${random}.png`;
+    if (random !== 1) {
+      currentScore += random;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener("click", () => {
+  if (gameOver) {
+    score[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+    if (score[activePlayer] >= 100) {
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      gameOver = false;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener("click", () => {
+  let currentScore = 0;
+  let activePlayer = 0;
+  let score = [0, 0];
+  let gameOver = true;
+  document.getElementById(`current--0`).textContent = 0;
+  document.getElementById(`current--1`).textContent = 0;
+  document.getElementById(`score--0`).textContent = 0;
+  document.getElementById(`score--1`).textContent = 0;
+  document.querySelector(".player--0").classList.remove("player--winner");
+  document.querySelector(".player--1").classList.remove("player--winner");
+  document.querySelector(".player--1").classList.remove("player--active");
+  document.querySelector(".player--0").classList.add("player--active");
+});
